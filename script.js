7073cc15bbc0a20acacfe14b2ee45b7adf2e53ef
Oguzhanmolu <oguzhanmolu@gmail.com>
2023-01-05 17:34:29 +0300

    added bookcard and book elements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 const btnAddBook = document.querySelector('#add-book');
 const bookModal = document.querySelector('#modal');
-const submitButton = document.querySelector('#submit-button');
+const btnSubmit = document.querySelector('#submit-button');
 const bookForm = document.querySelector('#form');
 const htmlMain = document.querySelector('#main');
 const bookDisplayArea = document.querySelector('#book-display-area');
@@ -12,15 +12,18 @@ let bookRead = document.querySelector('#book-read');
 let myLibrary = [];
 
 btnAddBook.addEventListener('click', () => {
-  bookModal.style.display = 'block';
   bookForm.reset();
-  htmlMain.style.filter = 'blur(2px)';
+  bookModal.style.display = 'block';
+  htmlMain.style.filter = 'blur(1px)';
 });
 
-submitButton.addEventListener('click', () => {
+btnSubmit.addEventListener('click', () => {
   bookModal.style.display = 'none';
   htmlMain.style.filter = 'blur(0px)';
-  addBookToLibrary();
+
+  bookTitle.value && bookAuthor.value && bookPages.value
+    ? addBookToLibrary()
+    : alert('Please fill all information');
 });
 
 function Book(title, author, pages, read) {
@@ -38,8 +41,35 @@ function addBookToLibrary() {
     bookRead.checked
   );
   myLibrary.push(newBook);
-  //
-  const newBookCard = document.createElement('div');
-  newBookCard.classList.add('book-card');
-  bookDisplayArea.appendChild(newBookCard);
+  createBookCard(newBook);
 }
+const createBookCard = (book) => {
+  const bookCard = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const read = document.createElement('button');
+  const removeBtn = document.createElement('button');
+
+  bookCard.classList.add('book-card');
+  if (book.read === true) {
+    read.textContent = 'Read';
+    read.classList.add('green-button');
+  } else {
+    read.textContent = 'Not Read';
+    read.classList.add('red-button');
+  }
+  removeBtn.classList.add('remove-button');
+
+  title.textContent = `${book.title}`;
+  author.textContent = `${book.author}`;
+  pages.textContent = `${book.pages}`;
+  removeBtn.textContent = 'Remove';
+
+  bookDisplayArea.appendChild(bookCard);
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(read);
+  bookCard.appendChild(removeBtn);
+};
